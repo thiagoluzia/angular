8cec9a6fb45aeadf0fc0970c060189fd9d11a5a6
Thiago Moura <thiago.mouraluzia@gmail.com>
2022-06-04 20:29:27 -0300

    subindo site

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,81 @@
 import { Component } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Todo } from 'src/models/todo.model';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', //<app-root>
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo';
+  public mode: string =  'list';
+  public todos: Todo[] = []; 
+  public title: string ='Minhas tarefas';
+  public form: FormGroup;
+
+
+  //construtor
+  constructor(private fb: FormBuilder) {
+    
+    this.form = this.fb.group({
+      title: ['', Validators.compose([
+        Validators.minLength(3),
+        Validators.maxLength(60),
+        Validators.required
+      ])]
+    });
+
+    this.load();
+  }
+
+  //metodos
+  remove(todo: Todo){
+    const index = this.todos.indexOf(todo);
+    
+    if(index !== -1){
+      this.todos.splice(index,1);
+    }
+    this.save();
+  }
+
+  markAsDone(todo: Todo){
+    todo.done = true;
+    this.save();
+  }
+
+  markAsUndone(todo: Todo){
+    todo.done = false;
+    this.save();
+  }
+
+  add(){
+    const title = this.form.controls['title'].value;
+    const id = this.todos.length +1;
+    this.todos.push(new Todo(id, title, false) );
+    this.save();
+    this.clear();
+  }
+  clear(){
+    this.form.reset();
+  }
+
+  //salvar em local storage 
+  save(){
+    const data = JSON.stringify(this.todos);
+    localStorage.setItem('todos', data);
+    this.mode='list'
+  }
+
+  load(){
+    const data = localStorage.getItem('todos');
+    if(data){
+      this.todos = JSON.parse(data);
+     }else{
+       this.todos = [];
+     }
+  }
+  changeMode(mode:string){
+    this.mode=mode;
+  }
 }
